f23ba3e89114c1a3f4abd2e8d2b82ae8d2687468
Skyler Spaeth <skylerspaeth@yandall.skylerspaeth.com>
2020-05-22 16:55:26 -0500

    removed deprecated routes file

--- server.js
+++
@@ -1,7 +0,0 @@
-const express = require('express'),
-	app = express();
-
-app.use(express.static(__dirname + '/public'));
-app.get('/bruh', (req, res) => { res.send('epicly you have hit /bruh'); console.log("bruh!"); return });
-
-app.listen(3000, console.log('listening'));
